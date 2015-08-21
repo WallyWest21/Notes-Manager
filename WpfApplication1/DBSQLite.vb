9e7ce0cbac4eb616d85f2c99a82da1333e0e0148
WallyWest21 <candymanis@hotmail.com>
2015-08-21 01:07:09 -0400

    SQLite Binding

---
+++ WpfApplication1/DBSQLite.vb
@@ -0,0 +1,96 @@
+﻿Imports System.Data
+Imports System.Data.SQLite
+
+Public Class DBSQLite
+    Dim ConnectionString As String
+    Dim m_dbConnection As SQLiteConnection
+
+    Public Sub Connect()
+
+        Try
+            SQLiteConnection.CreateFile("Notes Manager.sqlite")
+        Catch
+
+        End Try
+
+        m_dbConnection = New SQLiteConnection("Data Source= Notes Manager.sqlite; Version=3;")
+        m_dbConnection.Open()
+        Dim strSQL As String
+        Dim command As SQLiteCommand
+
+
+
+        Try
+            strSQL = "CREATE TABLE MainTag (ID int PRIMARY KEY , Designation Varchar(20), TileColor Varchar(20))"
+            command = New SQLiteCommand(strSQL, m_dbConnection)
+            command.ExecuteNonQuery()
+        Catch
+
+        End Try
+
+        strSQL = "INSERT INTO MainTag (ID, Designation, TileColor) VALUES (NULL, 'Material', 'DarkGoldenRod')"
+        command = New SQLiteCommand(strSQL, m_dbConnection)
+        command.ExecuteNonQuery()
+
+        Dim adapter As New SQLiteDataAdapter
+        Dim dataset As New DataSet()
+
+        m_dbConnection.Open()
+
+        command.CommandText = "SELECT * FROM MainTag"
+        adapter.SelectCommand = command
+        adapter.Fill(dataset, "MainTags")
+
+        adapter.Dispose()
+        command.Dispose()
+        m_dbConnection.Close()
+
+    End Sub
+
+    Public Function Binding() As DataSet
+
+        Try
+            SQLiteConnection.CreateFile("Notes Manager.sqlite")
+        Catch
+
+        End Try
+
+        m_dbConnection = New SQLiteConnection("Data Source= Notes Manager.sqlite; Version=3;")
+        m_dbConnection.Open()
+        Dim strSQL As String
+        Dim command As SQLiteCommand
+
+        Try
+            strSQL = "CREATE TABLE MainTag (ID int PRIMARY KEY , Designation Varchar(20), TileColor Varchar(20))"
+            command = New SQLiteCommand(strSQL, m_dbConnection)
+            command.ExecuteNonQuery()
+        Catch
+
+        End Try
+
+        strSQL = "INSERT INTO MainTag (ID, Designation, TileColor) VALUES (NULL, 'Material', 'DarkGoldenRod')"
+        command = New SQLiteCommand(strSQL, m_dbConnection)
+        command.ExecuteNonQuery()
+
+        strSQL = "INSERT INTO MainTag (ID, Designation, TileColor) VALUES (NULL, 'SpinPit', 'Crimson')"
+        command = New SQLiteCommand(strSQL, m_dbConnection)
+        command.ExecuteNonQuery()
+
+
+        Dim adapter As New SQLiteDataAdapter
+        Dim dataset As New DataSet()
+
+        'm_dbConnection.Open()
+
+        command.CommandText = "SELECT * FROM MainTag"
+        adapter.SelectCommand = command
+        adapter.Fill(dataset, "MainTags")
+
+        adapter.Dispose()
+        command.Dispose()
+        m_dbConnection.Close()
+
+        Return dataset
+
+    End Function
+End Class
---
+++ WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data
+Imports System
+
+Class MainWindow
+    Dim DB As New DBSQLite
+    Private Sub Tblk_Home_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Tblk_Home.MouseDown
+        'DB.Connect()
+
+    End Sub
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        MyListBox.ItemsSource = DB.Binding.Tables("MainTags").DefaultView
+    End Sub
+End Class
