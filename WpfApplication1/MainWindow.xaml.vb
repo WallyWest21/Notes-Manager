c2bfe60cd6b60bc36a41364f1153351599d779fe
WallyWest21 <candymanis@hotmail.com>
2015-08-22 13:27:34 -0400

    Add new UC Notes

--- WpfApplication1/MainWindow.xaml.vb
+++ WpfApplication1/MainWindow.xaml.vb
@@ -3,12 +3,17 @@ Imports System
 
 Class MainWindow
     Dim DB As New DBSQLite
+    Dim uc_MainTags As New MainTags
+    Dim uc_Notes As New Notes
     Private Sub Tblk_Home_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Tblk_Home.MouseDown
         'DB.Connect()
 
     End Sub
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
-        MyListBox.ItemsSource = DB.Binding.Tables("MainTags").DefaultView
+        'MyListBox.ItemsSource = DB.Binding.Tables("MainTags").DefaultView
+
+        'sp_MainBody.Children.Add(uc_MainTags)
+        sp_MainBody.Children.Add(uc_Notes)
     End Sub
 End Class
---
+++ WpfApplication1/Notes.xaml.vb
@@ -0,0 +1,7 @@
+﻿Public Class Notes
+
+    Dim DB As New DBSQLite
+    Private Sub UserControl_Loaded(sender As Object, e As RoutedEventArgs)
+        MyListBox.ItemsSource = DB.Binding.Tables("MainTags").DefaultView
+    End Sub
+End Class
