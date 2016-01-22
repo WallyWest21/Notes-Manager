c00b5791530b4ede9bbdbffa54e38f4b073a4c10
WallyWest21 <candymanis@hotmail.com>
2016-01-22 09:33:50 -0500

    Cleaned DBSQLite

--- WpfApplication1/DBSQLite.vb
+++ WpfApplication1/DBSQLite.vb
@@ -4,48 +4,6 @@ Imports System.Data.SQLite
 Public Class DBSQLite
     Dim ConnectionString As String
     Dim m_dbConnection As SQLiteConnection
-
-    '
-    '    Try
-    '        SQLiteConnection.CreateFile("Notes Manager.sqlite")
-    '    Catch
-
-    '    End Try
-
-    '    m_dbConnection = New SQLiteConnection("Data Source= Notes Manager.sqlite; Version=3;")
-    '    m_dbConnection.Open()
-    '    Dim strSQL As String
-    '    Dim command As SQLiteCommand
-
-
-
-    '    Try
-    '        strSQL = "CREATE TABLE MainTag (ID int PRIMARY KEY , Designation Varchar(20), TileColor Varchar(20))"
-    '        command = New SQLiteCommand(strSQL, m_dbConnection)
-    '        command.ExecuteNonQuery()
-    '    Catch
-
-    '    End Try
-
-    '    strSQL = "INSERT INTO MainTag (ID, Designation, TileColor) VALUES (NULL, 'Material', 'DarkGoldenRod')"
-    '    command = New SQLiteCommand(strSQL, m_dbConnection)
-    '    command.ExecuteNonQuery()
-
-    '    Dim adapter As New SQLiteDataAdapter
-    '    Dim dataset As New DataSet()
-
-    '    m_dbConnection.Open()
-
-    '    command.CommandText = "SELECT * FROM MainTag"
-    '    adapter.SelectCommand = command
-    '    adapter.Fill(dataset, "MainTags")
-
-    '    adapter.Dispose()
-    '    command.Dispose()
-    '    m_dbConnection.Close()
-
-    'End Sub
-
     Public Function Binding() As DataSet
 
         'Try
